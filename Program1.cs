b5d2e7544ee271e664bcaecf7b4f48543320bcc3
NANAME002 <dennzerogo@gmail.com>
2025-02-19 10:40:51 +0300

    Add files via upload

---
+++ Program1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+       {
+          /*  int[] mass =new int[Size()];
+            Massiv(mass);
+            Output(mass);
+            Console.ReadKey();
+        }
+        static int Size()
+        {
+            Console.WriteLine("n= ");
+            int n =int.Parse(Console.ReadLine());
+            return n;
+        }
+        static void Massiv(int[] mass)
+        {
+          
+            for (int i = 0; i < mass.Length; i++)
+            {
+                Console.WriteLine($"mass[{i}]");
+                mass[i]= int.Parse(Console.ReadLine());
+            }    
+            
+        }
+        static void Output(int[] mass)
+        {
+            for(int i = 0; i <mass.Length ;i++)
+            {
+                Console.WriteLine($"massiv[{i}]={mass[(i)]}");
+            }*/
+        }
+    }
+}
